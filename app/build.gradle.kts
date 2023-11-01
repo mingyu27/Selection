7e0e46ac1679e3bd3d4c0fb4e32e3bd9bd1944f3
seongmingyu <mingyu27@naver.com>
2023-11-01 19:54:58 +0900

    231101

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.kakao.sdk:v2-user:2.8.6") // 카카오 로그인
+    implementation ("com.kakao.sdk:v2-talk:2.6.0") // 친구, 메시지(카카오톡)
+    implementation ("com.kakao.sdk:v2-story:2.6.0") // 카카오스토리
+    implementation ("com.kakao.sdk:v2-link:2.6.0") // 메시지(카카오링크)
+    implementation ("com.kakao.sdk:v2-navi:2.6.0") // 카카오 내비
+// 오픈소스 이미지 핸들링할 때 많이 사용되는 글라이드
+    implementation ("com.github.bumptech.glide:glide:4.11.0")
 }
